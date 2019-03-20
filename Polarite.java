635855c5702ce7329ab5e8e9fdb8651b653d0098
Elodie-CH <Elodie-CH>
2019-03-20 14:29:58 +0100

    Create Polarite.java
    Add Enum Polarite

---
+++ Polarite.java
@@ -0,0 +1,15 @@
+
+public enum Polarite {
+	ABSENT("-"), PRESENT("+");
+	
+	private String sign = "";
+	
+	private Polarite(String sign){
+		this.sign = sign;
+	}
+	
+	@Override
+	public String toString(){
+		return sign;
+	}
+}
